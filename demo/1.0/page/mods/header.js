c158f276c2c8284427bbac63df05de7105ee16c6
dawncc <dawn.cc022@gmail.com>
2013-10-25 17:44:57 +0800

    测试augment的使用和mixin 特性
    Signed-off-by: dawncc <dawn.cc022@gmail.com>

--- demo/1.0/page/mods/header.js
+++ demo/1.0/page/mods/header.js
@@ -5,6 +5,23 @@
  **/
 KISSY.add(function(S) {
  
+  	/**
+  	 *  augment的使用和mixin 特性
+  	 */
+	var S = KISSY,
+  	Shoutable = { 
+  		shout: function() { alert("Iam a " + this.name + ".") }
+  	};
+
+  	function Dog(name) { this.name = "Dog " + name; };
+  	function Cat(name) { this.name = "Cat " + name; };
+
+  	S.augment(Dog, Shoutable);
+  	S.augment(Cat, Shoutable);
+
+  	new Dog('Jack').shout();
+  	new Cat('Tom').shout();
+
     return function(){
         alert('header of refund.');
     }
